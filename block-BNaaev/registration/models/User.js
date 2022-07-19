9f6e8464d3902c142d24bd3c2815078422e491a2
raj shringi <raj24shringi@gmail.com>
2022-07-19 21:20:46 +0530

    add age and phone-no. in schema

--- block-BNaaev/registration/models/User.js
+++ block-BNaaev/registration/models/User.js
@@ -6,6 +6,8 @@ const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlegth: 5 },
+  age: { type: Number },
+  phone: { type: Number },
 });
 
 userSchema.pre("save", function (next) {
